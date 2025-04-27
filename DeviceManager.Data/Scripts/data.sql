ba4c8b18f631469cd9da19fd9efdf4c6cb530693
s30882 <s30882@pjwstk.edu.pl>
2025-04-27 17:57:28 +0200

    Added script files to DeviceManager.Data

---
+++ DeviceManager.Data/Scripts/data.sql
@@ -0,0 +1,21 @@
+﻿-- Insert into Device table
+INSERT INTO Device (Id, Name, IsEnabled)
+VALUES
+    ('device1', 'Embedded Device 1', 1),
+    ('device2', 'Personal Computer 1', 1),
+    ('device3', 'Smartwatch 1', 1);
+
+-- Insert into EmbeddedDevice table
+INSERT INTO EmbeddedDevice (Id, Ip, NetworkName)
+VALUES
+    ('device1', '192.168.0.10', 'Network1');
+
+-- Insert into PersonalComputer table
+INSERT INTO PersonalComputer (Id, OS)
+VALUES
+    ('device2', 'Windows 10');
+
+-- Insert into Smartwatch table
+INSERT INTO Smartwatch (Id, Power)
+VALUES
+    ('device3', 85);
---
+++ DeviceManager.Data/Scripts/scheme.sql
@@ -0,0 +1,32 @@
+﻿-- Create Device table
+CREATE TABLE Device (
+                        Id NVARCHAR(50) PRIMARY KEY,
+                        Name NVARCHAR(100) NOT NULL,
+                        IsEnabled BIT NOT NULL
+);
+GO
+
+    -- Create EmbeddedDevice table
+CREATE TABLE EmbeddedDevice (
+                                Id NVARCHAR(50) PRIMARY KEY,
+                                Ip NVARCHAR(15) NOT NULL,
+                                NetworkName NVARCHAR(100) NOT NULL,
+                                FOREIGN KEY (Id) REFERENCES Device(Id) ON DELETE CASCADE
+);
+GO
+
+    -- Create PersonalComputer table
+CREATE TABLE PersonalComputer (
+                                  Id NVARCHAR(50) PRIMARY KEY,
+                                  OS NVARCHAR(100) NOT NULL,
+                                  FOREIGN KEY (Id) REFERENCES Device(Id) ON DELETE CASCADE
+);
+GO
+
+    -- Create Smartwatch table
+CREATE TABLE Smartwatch (
+                            Id NVARCHAR(50) PRIMARY KEY,
+                            Power BIGINT NOT NULL,
+                            FOREIGN KEY (Id) REFERENCES Device(Id) ON DELETE CASCADE
+);
+GO
